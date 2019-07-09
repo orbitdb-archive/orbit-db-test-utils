1efc46c0f4220b118ec7889a2ce5b299054161cb
Mark Henderson <henderson.mark@gmail.com>
2019-07-09 16:29:15 -0400

    Upgrades for new versions of ipfsd-ctl and go-ipfs-dep

--- start-ipfs.js
+++ start-ipfs.js
@@ -17,35 +17,34 @@ const repoConf = {
  * @param  {Object}  config  [IPFS configuration to use]
  * @return {[Promise<IPFS>]} [IPFS instance]
  */
-const startIpfs = (type, config = {}) => {
-  return new Promise((resolve, reject) => {
-    if (!testAPIs[type]) {
-      reject(new Error(`Wanted API type ${JSON.stringify(type)} is unknown. Available types: ${Object.keys(testAPIs).join(', ')}`))
-    }
+const startIpfs = async (type, config = {}) => {
+  if (!testAPIs[type]) {
+    throw new Error(`Wanted API type ${JSON.stringify(type)} is unknown. Available types: ${Object.keys(testAPIs).join(', ')}`)
+  }
 
-    // Use custom storage backend for IPFS block store
-    // NOTE: need to duck-type 'repoPath' instead of 'repo' as ipfsd-ctl
-    // flips the two, so passing 'repo' doesn't work as it does with js-ipfs
-    if (isNode) {
-      // Use DatastoreLevel only in Nodejs environment
-      config = Object.assign({}, config, { repoPath: new IPFSRepo(config.repo, repoConf) })
-    }
+  // Use custom storage backend for IPFS block store
+  // NOTE: need to duck-type 'repoPath' instead of 'repo' as ipfsd-ctl
+  // flips the two, so passing 'repo' doesn't work as it does with js-ipfs
+  if (isNode) {
+    // Use DatastoreLevel only in Nodejs environment
+    config = Object.assign({}, config, { repoPath: new IPFSRepo(config.repo, repoConf) })
+  }
 
-    // If we're starting a process, pass command line arguments to it
-    if (!config.args) {
-      config.args = ['--enable-pubsub-experiment']
-    }
+  // If we're starting a process, pass command line arguments to it
+  if (!config.args) {
+    config.args = ['--enable-pubsub-experiment']
+  }
 
-    // Spawn an IPFS daemon (type defined in)
-    IPFSFactory
-      .create(testAPIs[type])
-      .spawn(config, async (err, ipfsd) => {
-        if (err) {
-          return reject(err)
-        }
-        resolve(ipfsd.api)
-      })
-  })
+  // Spawn an IPFS daemon (type defined in)
+  try {
+    const f = IPFSFactory.create(testAPIs[type])
+    const ipfsd = await f.spawn(config)
+    debugger;
+    return ipfsd.api;
+  } catch(err) {
+    throw new Error(err)
+  }
+
 }
 
 module.exports = startIpfs
