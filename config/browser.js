7bb151d4f4349f6390f7f15dd0501aa6f2e78f88
Mark Henderson <mark@mrh.io>
2020-09-16 17:54:53 -0400

    removing default repo location

--- config/browser.js
+++ config/browser.js
@@ -8,7 +8,6 @@ module.exports = {
     preload: {
       enabled: false
     },
-    repo: './ipfs/ipfs-log/tests/daemon',
     start: true,
     EXPERIMENTAL: {
       pubsub: true
@@ -32,7 +31,6 @@ module.exports = {
     }
   },
   daemon1: {
-    repo: './ipfs/ipfs-log/tests/daemon1',
     start: true,
     relay: { enabled: true, hop: { enabled: true, active: true } },
     EXPERIMENTAL: {
@@ -57,7 +55,6 @@ module.exports = {
     }
   },
   daemon2: {
-    repo: './ipfs/ipfs-log/tests/daemon2',
     start: true,
     relay: { enabled: true, hop: { enabled: true, active: true } },
     EXPERIMENTAL: {
