ee9f412aaddc5d4b3c7456cf53a99f39321cf826
Mark Henderson <henderson.mark@gmail.com>
2021-02-16 11:07:50 -0500

    cleanup: removing commented out code

--- index.js
+++ index.js
@@ -7,12 +7,3 @@ exports.startIpfs = require('./start-ipfs')
 exports.stopIpfs = require('./stop-ipfs')
 exports.testAPIs = require('./test-apis.js')
 exports.waitForPeers = require('./wait-for-peers')
-
-// Different abstract-leveldown implementations
-// const implementations = require('./implementations')
-// const properLevelModule = implementations
-//   .filter(i => i.key.indexOf('level') > -1)
-//   .map(i => i.module)[0]
-// const defaultStorage = require('orbit-db-storage-adapter')(properLevelModule)
-// exports.defaultStorage = defaultStorage
-// exports.implementations = implementations
