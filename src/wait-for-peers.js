6a888676cb3b8ffca560d62ec8b0a1a6978cfd49
Mark Henderson <mark@mrh.io>
2022-07-13 09:33:12 +0000

    fix: use peer.toString for waitForPeers

--- src/wait-for-peers.js
+++ src/wait-for-peers.js
@@ -5,7 +5,10 @@ const waitForPeers = (ipfs, peersToWait, topic) => {
     const interval = setInterval(async () => {
       try {
         const peers = await ipfs.pubsub.peers(topic)
-        const hasAllPeers = peersToWait.map((e) => peers.includes(e)).filter((e) => e === false).length === 0
+        const peerIds = peers.map(peer => peer.toString())
+        const peerIdsToWait = peersToWait.map(peer => peer.toString())
+
+        const hasAllPeers = peerIdsToWait.map((e) => peerIds.includes(e)).filter((e) => e === false).length === 0
 
         // FIXME: Does not fail on timeout, not easily fixable
         if (hasAllPeers) {
